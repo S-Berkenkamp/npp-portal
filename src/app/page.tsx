dd6b2c5158c068c88644adcb569d07fde5edaa83
NPP-Projekt <shop.berkenkamp@gmx.de>
2025-09-20 00:31:27 +0200

    chore(portal): sichtbarer homepage-text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,14 @@ export default function Home() {
         <Image
           className="dark:invert"
           src="/next.svg"
-          alt="Next huhu.js logo"
+          alt="Next.js logo"
           width={180}
           height={38}
           priority
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by hihuhu{" "}
+            Get puups {" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               src/app/page.tsx
             </code>
@@ -39,7 +39,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Depl..
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
