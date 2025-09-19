3364e4735689bd538c9bc88333aea5ec323fa5f0
NPP-Projekt <shop.berkenkamp@gmx.de>
2025-09-20 01:00:38 +0200

    chore(portal): sichtbarer CI-CD test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function Home(huhu) {
+export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -16,7 +16,7 @@ export default function Home(huhu) {
           <li className="mb-2 tracking-[-.01em]">
             Get puups {" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
+              src/lololo/page.tsx
             </code>
             .
           </li>
